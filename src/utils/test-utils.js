aa1529f82cf4eaaa1ce3f14a66377043f8015ee8
Tony Bradley <antbradley91@gmail.com>
2017-04-02 09:59:46 -0400

    fix cssauron

---
+++ src/utils/test-utils.js
@@ -0,0 +1,44 @@
+const css = require('cssauron')({
+  id: 'id',
+  'class': 'className',
+  parent: 'parentNode',
+  tag: function (node) { return (node.tagName || node.nodeName || '').toLowerCase() },
+  contents: function (node) { return node.innerText || node.textContent || '' },
+  children: function (node) {
+    if (node.children) {
+      return node.children
+    }
+
+    var out = []
+    for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+      if (node.childNodes[i].tagName) out.push(node.childNodes[i])
+    }
+    return out
+  },
+  attr: function (node, attr) { return node.getAttribute(attr) }
+})
+const walk = require('dom-walk')
+
+function getSelector (query) {
+  const selector = css(query)
+  return function check (element) {
+    if (check.found) return
+    check.found = selector(element)
+  }
+}
+
+function find (queries, dom) {
+  const selectors = (Array.isArray(queries) ? queries : [queries]).map(getSelector)
+  walk(dom, function (current) {
+    for (var i = 0; i < selectors.length; i++) {
+      if (current.getAttribute) selectors[i](current)
+    }
+  })
+
+  return selectors.map(s => s.found)
+}
+
+module.exports = {
+  getSelector,
+  find
+}
--- src/views/home.test.js
+++ src/views/home.test.js
@@ -1,9 +1,26 @@
-require('babel-register')
-require('undom/register')
 const test = require('ava').test
+const {h, render} = require('preact')
+const {find} = require('../utils/test-utils')
+const home = require('./home')
+
+// initialize a fresh store before each test
+test.beforeEach(function (t) {
+  t.context.store = require('socrates')({
+    home: home.model
+  })
+  t.context.store({type: 'init:home'})
+})
 
 test('home view', function (t) {
-  const home = require('./home')
-  home.view({})
+  const store = t.context.store
+  render(
+    h(home.view, {state: store(), dispatch: store}),
+    document.body
+  )
+
+  const results = find('input', document.body)
+
+  console.log(results[0])
+
   t.pass()
 })
