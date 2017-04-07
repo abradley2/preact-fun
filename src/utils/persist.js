3a5136b8f2e6252e7a0f75b340a6670c8f7f2e8b
Tony Bradley <antbradley91@gmail.com>
2017-04-07 07:27:49 -0400

    clean up persist

--- src/utils/persist.js
+++ src/utils/persist.js
@@ -11,6 +11,7 @@ function createWriter (namespace) {
 }
 
 function wrap (store) {
+  // wrap the store so it saves on every action dispatch
   const wrapped = function (payload) {
     if (!payload) return store()
     const actionTarget = payload.type.split(/[: .]/)[1]
@@ -18,7 +19,9 @@ function wrap (store) {
     if (!writers[actionTarget]) createWriter(actionTarget)
     writers[actionTarget](store())
   }
+  // mimick redux store api
   wrapped.subscribe = store.subscribe.bind(store)
+  wrapped.getState = store
   return wrapped
 }
 
@@ -26,6 +29,8 @@ function load (store) {
   const initialState = store()
 
   return Promise.resolve()
+    // load previous state. Check if there was a change, use this to determine if
+    // the stored state should be loaded
     .then(function () {
       const getPrevState = defer()
 
@@ -33,6 +38,7 @@ function load (store) {
         if (err) return getPrevState.reject(err)
 
         if (!equal(indexByNs(prevState), initialState)) {
+          console.log('different states: ', indexByNs(prevState), initialState)
           return getPrevState.resolve(false)
         }
 
@@ -41,37 +47,46 @@ function load (store) {
 
       return getPrevState.promise
     })
+    // if the stored state should be loaded, then do so. Otherwise skip
+    // to end of this
     .then(function (shouldLoadState) {
-      if (shouldLoadState) {
-        const loadStoredState = defer()
-        db.store('storedState').all(function (err, data) {
-          if (err) return loadStoredState.reject(err)
-          data.forEach(function (item) {
-            store({type: 'set ' + item.namespace, payload: item})
-          })
-          return loadStoredState.resolve()
+      if (!shouldLoadState) return Promise.resolve()
+      const loadStoredState = defer()
+      db.store('storedState').all(function (err, data) {
+        if (err) return loadStoredState.reject(err)
+        data.forEach(function (item) {
+          store({type: 'set ' + item.namespace, payload: item})
         })
-        return loadStoredState.promise
-      }
-      return Promise.resolve()
+        return loadStoredState.resolve()
+      })
+      return loadStoredState.promise
     })
+    // the current state of the store should be stored as the prevState
+    // so we can compare it in the previous block next time
     .then(function () {
       const savePrevState = defer()
+      const newInitialState = store()
 
-      db.store('prevState').batch(values(initialState), function (err) {
+      db.store('prevState').batch(values(newInitialState), function (err) {
         if (err) return savePrevState.reject(err)
         return savePrevState.resolve()
       })
       return savePrevState.promise
     })
+    // return a wrapped store so it will save state on dispatch
     .then(function () {
       return Promise.resolve(wrap(store))
     })
     .catch(function (err) {
+      // if something went wrong, log the error, and clear the stores
       console.error(err)
+      db.store('storedState').clear(Function.prototype)
+      db.store('prevState').clear(Function.prototype)
       return Promise.resolve(store)
     })
 }
+
+// Utility functions
 
 function bottleneck (func, time) {
   var lastArgs = []
